9a2ab7fbe7253a64664c1c52e777a2105ba5760b
Tushar Chemate <41479583+tusharchemate@users.noreply.github.com>
2024-12-21 15:25:13 +0530

    Create reverse.js

---
+++ code /reverse.js
@@ -0,0 +1,14 @@
+// Reverse a String
+const reverseString = (str) => {
+	
+  let rev = "";
+  
+  for(let i= str.length-1; i>=0; i--) {
+  
+  rev += str[i];
+  
+  } 
+  return rev;
+}
+
+console.log(reverseString('Tushar'));
